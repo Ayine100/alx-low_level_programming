51b13577d8833a5689affdae6e4b0c088d5fbfbe
“Your <Akanlobe@gmail.com>
2023-03-02 15:23:01 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/* 
+ * C program to print a long int number
+ * @n: number to be printed
+ * return: 0
+ */
+#include <stdio.h>
+ 
+void print(long n)
+{
+	// If number is smaller than 0, put a - sign
+	// and change number to positive
+	if (n < 0) 
+	{
+	putchar('-');
+	n = -n;
+	}
+ 
+	// Remove the last digit and recur
+	if (n/10)
+	print(n/10);
+ 
+	// Print the last digit
+	putchar(n%10 + '0');
+ 
+	// Driver program to test above function
+	int main();
+
+	long int n = 12045;
+	print(n);
+	return 0;
+}
